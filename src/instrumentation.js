c703d5f2836f22c7804c416149e50a81e4aec1d9
Ali <alihussainsagir53@gmail.com>
2026-08-16 17:13:03 +0300

    refactor: remove automated node-cron background instrumentation

--- src/instrumentation.js
+++
@@ -1,7 +0,0 @@
-export async function register() {
-  if (process.env.NEXT_RUNTIME === 'nodejs') {
-    console.log('[KeepPulse] Initializing Node.js server instrumentation & background scheduler...');
-    const { initBackgroundScheduler } = await import('@/lib/scheduler/scheduler.js');
-    initBackgroundScheduler();
-  }
-}
